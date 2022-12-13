1f3d87966c78fa7e0bc885ff6ab4805f6e680d13
kainan <102630674+KAINANGONCALVES@users.noreply.github.com>
2022-12-12 21:15:55 -0300

    first commit

---
+++ mobile-navbar.js
@@ -0,0 +1,15 @@
+class mobilenavbar{
+        constructor(mobilemenu, navlist, navlinks) {
+            this.mobilemenu = document.querySelector(mobilemenu);
+            this .mobilemenu = document.querySelector(navlist);
+            this.mobilemenu = document.querySelector(navlinks);
+            this.activeclass = "active"
+        }
+
+    addClickEvent() {
+        this.mobilemenu.addEventListener"click",() => console.log
+    }
+
+
+
+}
